a85dd77af710fb76803f6debb4e7065b24131df2
David-123 <1826013250@qq.com>
2026-04-19 15:46:34 +0800

    feat(util.py): added log functions

--- util.py
+++ util.py
@@ -35,6 +35,7 @@ def get_power(room_id: str) -> Tuple[float, str]:
         return float(data["quantity"]), data["quantityunit"]
     return -1, resp["returnmsg"]
 
+
 def get_details(student_id: str, op_type: int, area_id: str = "0", build_id: str = "0", level_id: str = "0") -> list | dict:
     data = {
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
@@ -57,6 +58,19 @@ def get_details(student_id: str, op_type: int, area_id: str = "0", build_id: str
         return resp["businessData"]
     return resp
 
+
+def log_info(*args, sep=" ", end="\n"):
+    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}/INFO] ", *args, sep=sep, end=end)
+
+
+def log_error(*args, sep=" ", end="\n"):
+    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}/ERROR] ", *args, sep=sep, end=end)
+
+
+def log_warning(*args, sep=" ", end="\n"):
+    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}/WARNING] ", *args, sep=sep, end=end)
+
+
 class TimeUnits(Enum):
     MILLISECONDS = "毫秒"
     SECONDS = "秒"
@@ -64,11 +78,13 @@ class TimeUnits(Enum):
     HOURS = "小时"
     DAYS = "天"
 
+
 class RunnableSignals(QObject):
     signal_dorm_info_request_finished = Signal(object, object)
     signal_power_request_finished = Signal(object, object)
 
-class RunnableGetDromInfo(QRunnable):
+
+class RunnableGetDormInfo(QRunnable):
     def __init__(self, student_id: str, data: dict):
         super().__init__()
         self.data = data
@@ -79,6 +95,7 @@ class RunnableGetDromInfo(QRunnable):
         r = get_details(self.student_id, self.data["op_type"] + 1, self.data["area_id"], self.data["build_id"], self.data["level_id"])
         self.signal.signal_dorm_info_request_finished.emit(r, self.data)
 
+
 class RunnableGetPowerInfo(QRunnable):
     def __init__(self, dorm_id: str):
         super().__init__()
@@ -88,6 +105,7 @@ class RunnableGetPowerInfo(QRunnable):
     def run(self):
         q, qu = get_power(self.dorm_id)
         self.signal.signal_power_request_finished.emit(q, qu)
+
 
 class Settings:
     def __init__(self, startup_on_login: bool, query_interval: float, query_interval_unit: TimeUnits,
